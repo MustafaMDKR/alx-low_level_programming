07206870b8c1ae300cdcd706b9441babf476b351
MustafaMDKR <mustafamdkr@gmail.com>
2023-02-21 18:16:26 -0800

    05

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - entry point
+ * Description: print the sign of int n or 0
+ * @n: the int to be checked
+ * Return: + | - | 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	} else {
+		_putchar('0');
+		return (0);
+	}
+}
