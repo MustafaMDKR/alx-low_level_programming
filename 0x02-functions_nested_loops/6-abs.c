4152f772329de1bf6b74a2fc06b8b43716bb9c88
MustafaMDKR <mustafamdkr@gmail.com>
2023-02-21 18:26:35 -0800

    06

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - entry point
+ * Description: check the absolute value of int
+ * @n: int to be checked
+ * Return: n | -n
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
